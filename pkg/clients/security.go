10c8110b359b3793237ad1df81dddf596f693332
Jeff Cantrill <jcantril@redhat.com>
2019-06-18 16:25:26 -0400

    mget load of security documents

--- pkg/clients/security.go
+++ pkg/clients/security.go
@@ -10,8 +10,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	securityIndex   = ".security"
+	securityDocType = "security"
+)
+
 //ESSecurityClient for reading and writing security documents
-type ESSecurityClient interface {
+type SecurityClient interface {
+	FetchACLs() (*security.ACLDocuments, error)
 
 	//FetchRolesMapping for security
 	FetchRolesMapping() (*security.RolesMapping, error)
@@ -29,7 +35,7 @@ type DefaultESSecurityClient struct {
 }
 
 //NewESSecurityClient initializes the client
-func NewESSecurityClient(opts ext.Options) (ESSecurityClient, error) {
+func NewESSecurityClient(opts ext.Options) (SecurityClient, error) {
 	if opts.UpstreamURL == nil {
 		return nil, fmt.Errorf("The UpstreamURL proxy URL is nil")
 	}
@@ -58,13 +64,21 @@ func decodeACLDocument(resp, docType string) (string, error) {
 	return string(unencoded), nil
 }
 
-func (sg *DefaultESSecurityClient) FetchRoles() (*security.Roles, error) {
-	log.Debug("Fetching Security roles...")
-	resp, err := sg.esClient.Get("/.security/security/roles")
+func decodeACLDocumentFrom(item MGetResponseItem, docType string) (string, error) {
+	log.Tracef("Decoding docType %q from %v", docType, item)
+	source := item.Source[docType]
+	log.Tracef("ACLDocument _source to decode: %v", source)
+	unencoded, err := base64.StdEncoding.DecodeString(source.(string))
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	sRoles, err := decodeACLDocument(resp, "roles")
+
+	return string(unencoded), nil
+}
+
+func decodeRolesACLDocumentFrom(item MGetResponseItem) (*security.Roles, error) {
+	log.Tracef("Decoding Roles from %v", item)
+	sRoles, err := decodeACLDocumentFrom(item, security.DocTypeRoles)
 	if err != nil {
 		return nil, err
 	}
@@ -76,14 +90,21 @@ func (sg *DefaultESSecurityClient) FetchRoles() (*security.Roles, error) {
 	log.Debugf("Roles: %s", sRoles)
 	return roles, nil
 }
-
-func (sg *DefaultESSecurityClient) FetchRolesMapping() (*security.RolesMapping, error) {
-	log.Debug("Fetching Security rolesmapping...")
-	resp, err := sg.esClient.Get("/.security/security/rolesmapping")
+func decodeRolesACLDocument(resp string) (*security.Roles, error) {
+	sRoles, err := decodeACLDocument(resp, security.DocTypeRoles)
 	if err != nil {
 		return nil, err
 	}
-	sRolesMapping, err := decodeACLDocument(resp, "rolesmapping")
+	roles := &security.Roles{}
+	err = roles.FromJson(sRoles)
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("Roles: %s", sRoles)
+	return roles, nil
+}
+func decodeRolesmappingACLDocument(resp string) (*security.RolesMapping, error) {
+	sRolesMapping, err := decodeACLDocument(resp, security.DocTypeRoles)
 	if err != nil {
 		return nil, err
 	}
@@ -94,6 +115,75 @@ func (sg *DefaultESSecurityClient) FetchRolesMapping() (*security.RolesMapping, 
 	}
 	log.Debugf("Rolesmapping: %s", sRolesMapping)
 	return rolesmapping, nil
+}
+func decodeRolesmappingACLDocumentFrom(item MGetResponseItem) (*security.RolesMapping, error) {
+	sRolesMapping, err := decodeACLDocumentFrom(item, security.DocTypeRolesmapping)
+	if err != nil {
+		return nil, err
+	}
+	rolesmapping := &security.RolesMapping{}
+	err = rolesmapping.FromJson(sRolesMapping)
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("Rolesmapping: %s", sRolesMapping)
+	return rolesmapping, nil
+}
+
+func (sg *DefaultESSecurityClient) FetchACLs() (*security.ACLDocuments, error) {
+	log.Debug("FetchACLs ACLs...")
+	items := MGetRequest{
+		Docs: []MGetItem{
+			MGetItem{
+				Type: securityDocType,
+				Id:   security.DocTypeRoles,
+			},
+			MGetItem{
+				Type: securityDocType,
+				Id:   security.DocTypeRolesmapping,
+			},
+		},
+	}
+	resp, err := sg.esClient.MGet(securityIndex, items)
+	if err != nil {
+		return nil, err
+	}
+	docs := &security.ACLDocuments{}
+	for _, item := range resp.Docs {
+		switch item.Id {
+		case security.DocTypeRoles:
+			doc, err := decodeRolesACLDocumentFrom(item)
+			if err != nil {
+				return nil, err
+			}
+			docs.Roles = *doc
+		case security.DocTypeRolesmapping:
+			doc, err := decodeRolesmappingACLDocumentFrom(item)
+			if err != nil {
+				return nil, err
+			}
+			docs.RolesMapping = *doc
+		}
+	}
+	return docs, nil
+}
+
+func (sg *DefaultESSecurityClient) FetchRoles() (*security.Roles, error) {
+	log.Debug("Fetching Security roles...")
+	resp, err := sg.esClient.Get("/.security/security/roles")
+	if err != nil {
+		return nil, err
+	}
+	return decodeRolesACLDocument(resp)
+}
+
+func (sg *DefaultESSecurityClient) FetchRolesMapping() (*security.RolesMapping, error) {
+	log.Debug("Fetching Security rolesmapping...")
+	resp, err := sg.esClient.Get("/.security/security/rolesmapping")
+	if err != nil {
+		return nil, err
+	}
+	return decodeRolesmappingACLDocument(resp)
 }
 
 func encodeACLDocument(doc security.Serializable) (string, error) {
